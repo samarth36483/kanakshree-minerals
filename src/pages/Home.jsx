bd8d716fa111b5e109ef6ad94e456ed6e41bbfc9
Samarth Raipuriya <samarthraipuriya@Samarths-MacBook-Air.local>
2025-05-17 17:30:39 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+      <h1 className='text-3xl font-bold text-gray-800'>Kanakshree Minerals Coming Soon</h1>
+    </div>
+  );
+}
